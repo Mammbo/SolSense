0425148af32e5d7c97349c9a5051bca0256be43a
Mammbo <113815614+Mammbo@users.noreply.github.com>
2024-10-06 14:22:16 -0500

    idk

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -6,8 +6,6 @@ import { auth } from "@clerk/nextjs/server";
 export async function getMyGeoData() {
 
     const user = auth();
-    if (!user.userId) throw new Error("Unauthorized");
-
     const geoData = db.query.geoData.findMany({
         where: (model, { eq }) => eq(model.userId, user.userId),
         orderBy: (model, { desc }) => desc(model.id)
