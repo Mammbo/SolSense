c4ff9771484e1cc540121d28b3606127235c3ebd
Mammbo <113815614+Mammbo@users.noreply.github.com>
2024-10-05 23:30:56 -0500

    break queries into single file

---
+++ src/server/queries.ts
@@ -0,0 +1,17 @@
+import "server-only";
+import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
+
+// this function is used to get the geo data of the user (for future project use)
+export async function getMyGeoData() {
+
+    const user = auth();
+    if (!user.userId) throw new Error("Unauthorized");
+
+    const geoData = db.query.geoData.findMany({
+        where: (model, { eq }) => eq(model.userId, user.userId),
+        orderBy: (model, { desc }) => desc(model.id)
+      });
+    return geoData;
+}
+
